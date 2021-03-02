5730ed15d420293ff7b13506ff00097f68a7e282
Andressa Nowasyk <andressanowasyk@gmail.com>
2021-03-02 07:19:33 -0300

    Menu criado

--- desafio-framework-andressanowasyk/Program.cs
+++ desafio-framework-andressanowasyk/Program.cs
@@ -8,5 +8,32 @@ namespace desafio_framework_andressanowasyk
         {
             string opcaoUsuario = ObterOpcaoUsuario();
         }
+
+        private static string ObterOpcaoUsuario() {
+            
+            Console.WriteLine();
+            Console.WriteLine("Desafio FrameWork Padawans 2021");
+            Console.WriteLine("May the Force be With You");
+            Console.WriteLine("Escolha Sabiamente uma Opção Abaixo");
+            Console.WriteLine();
+            Console.WriteLine("1 - Listar Todos Posts");
+            Console.WriteLine("2 - Listar Todos Albums");
+            Console.WriteLine("3 - Listar Todos ToDos");
+            Console.WriteLine("4 - Listar Usuários");
+            Console.WriteLine("5 - Listar Posts, Albums, ToDos por Usuário");
+            Console.WriteLine("6 - Adicionar Posts");
+            Console.WriteLine("7 - Adicionar Albums");
+            Console.WriteLine("8 - Adicionar ToDos");
+            Console.WriteLine("9 - Adicionar Usuario");
+            Console.WriteLine("C - Limpar Tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+
+            return opcaoUsuario;
+
+        }
     }
 }
